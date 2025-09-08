9af40bf64c82b82cb649c766356ac2e62216edd0
stephansama <stephanrandle@hotmail.com>
2025-09-08 17:57:24 -0400

    chore: delete types folder

--- types/wezterm/serde/init.lua
+++
@@ -1,9 +0,0 @@
----@meta
-
----@class Serde
----@field json_decode fun(t: string): table<string, ANY>
----@field json_encode fun(t: table<string, ANY>): string
----@field toml_decode fun(t: string): table<string, ANY>
----@field toml_encode fun(t: table<string, ANY>): string
----@field yaml_decode fun(t: string): table<string, ANY>
----@field yaml_encode fun(t: table<string, ANY>): string
